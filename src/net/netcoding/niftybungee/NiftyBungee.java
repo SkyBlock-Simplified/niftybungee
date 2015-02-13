b1125ca19895c9105cf7c9a8fe6812b45bb63546
Brian Graham <support@netcoding.net>
2015-02-13 15:38:02 -0500

    Properly handle reload and server restart

--- src/net/netcoding/niftybungee/NiftyBungee.java
+++ src/net/netcoding/niftybungee/NiftyBungee.java
@@ -6,12 +6,22 @@ import net.netcoding.niftybungee.minecraft.BungeeHelper;
 public class NiftyBungee extends Plugin {
 
 	private static transient NiftyBungee plugin;
+	private static transient BungeeHelper listener;
 
 	@Override
 	public void onEnable() {
 		plugin = this;
 		this.getProxy().registerChannel("NiftyBungee");
-		this.getProxy().getPluginManager().registerListener(this, new BungeeHelper());
+		this.getProxy().getPluginManager().registerListener(this, listener = new BungeeHelper());
+	}
+
+	@Override
+	public void onDisable() {
+		this.getProxy().unregisterChannel("NiftyBungee");
+		BungeeHelper.stopThread();
+		this.getProxy().getPluginManager().unregisterListener(listener);
+		listener = null;
+		
 	}
 
 	public static NiftyBungee getPlugin() {

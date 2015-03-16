8cf52ee5f49445f2c2db0b5791fb4734b254a249
Brian Graham <support@netcoding.net>
2015-03-16 15:03:50 -0400

    90% Console spam intercept

--- src/net/netcoding/niftybungee/NiftyBungee.java
+++ src/net/netcoding/niftybungee/NiftyBungee.java
@@ -1,9 +1,10 @@
 package net.netcoding.niftybungee;
 
+import java.io.PrintStream;
 import java.util.logging.Handler;
-import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
+import net.md_5.bungee.BungeeCord;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.netcoding.niftybungee.minecraft.BungeeHelper;
@@ -13,10 +14,15 @@ public class NiftyBungee extends Plugin {
 	private static transient NiftyBungee plugin;
 	private static transient BungeeHelper listener;
 
+	static {
+		System.setOut(new SystemInterceptor());
+	}
+
 	@Override
 	public void onEnable() {
 		plugin = this;
 		ProxyServer.getInstance().getLogger().addHandler(new LogHandler());
+		BungeeCord.getInstance().getLogger().addHandler(new LogHandler());
 		this.getProxy().registerChannel(BungeeHelper.NIFTY_CHANNEL);
 		this.getProxy().getPluginManager().registerListener(this, listener = new BungeeHelper());
 	}
@@ -33,6 +39,25 @@ public class NiftyBungee extends Plugin {
 		return plugin;
 	}
 
+	private static class SystemInterceptor extends PrintStream {
+
+		public SystemInterceptor() {
+			super(System.out, true);
+		}
+
+		@Override
+		public void print(String s) {
+			if (!s.contains("read timed out") && !s.contains("Command not found"))
+				super.print(s);
+		}
+
+		@Override
+		public void println(String x) {
+			if (!x.contains("read timed out") && !x.contains("Command not found"))
+				super.println(x);
+		}
+	}
+
 	private class LogHandler extends Handler {
 
 		@Override
@@ -44,10 +69,10 @@ public class NiftyBungee extends Plugin {
 		@Override
 		public void publish(LogRecord record) {
 			if (BungeeHelper.BUNGEE_CHANNEL.equals(record.getLoggerName())) {
-				if (Level.WARNING.equals(record.getLevel())) {
-					if (record.getMessage().endsWith("read timed out"))
-						record.setMessage("");
-				}
+				if (record.getMessage().contains("read timed out"))
+					record.setMessage(null);
+				else if (record.getMessage().contains("Command not found"))
+					record.setMessage(null);
 			}
 		}
 

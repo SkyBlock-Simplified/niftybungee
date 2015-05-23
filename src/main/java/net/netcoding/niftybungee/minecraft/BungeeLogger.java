3f0de988198085fcb6c9966717e375e83c788431
Brian Graham <support@netcoding.net>
2015-05-23 01:55:27 -0400

    Created a substitute logger since BungeeCord loads plugins backwards

--- src/main/java/net/netcoding/niftybungee/minecraft/BungeeLogger.java
+++ src/main/java/net/netcoding/niftybungee/minecraft/BungeeLogger.java
@@ -1,15 +1,27 @@
 package net.netcoding.niftybungee.minecraft;
 
+import java.io.InputStream;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.plugin.PluginDescription;
 import net.netcoding.niftycore.minecraft.MinecraftLogger;
 import net.netcoding.niftycore.util.StringUtil;
+
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.introspector.PropertyUtils;
 
 public class BungeeLogger extends MinecraftLogger {
 
 	public BungeeLogger(Plugin plugin) {
-		super(plugin.getLogger());
+		super(new BungeePluginLogger(plugin));
 	}
 
 	public void error(CommandSender sender, Object... args) {
@@ -29,13 +41,59 @@ public class BungeeLogger extends MinecraftLogger {
 	}
 
 	private void message(CommandSender sender, String message, Throwable exception, Object... args) {
-		boolean isConsole = ProxyServer.getInstance().getConsole().equals(sender); // TODO
+		boolean isConsole = BungeeHelper.isConsole(sender);
 
 		if ((isConsole || exception != null) || (isConsole && exception == null))
 			console(message, exception, args);
 
 		if (!isConsole)
-			sender.sendMessage(parse(message, args));
+			sender.sendMessage(TextComponent.fromLegacyText(parse(message, args)));
+	}
+
+	private static class BungeePluginLogger extends Logger {
+
+		private final static Yaml yaml;
+		private final String pluginName;
+
+		static {
+			Constructor yamlConstructor = new Constructor();
+	        PropertyUtils propertyUtils = yamlConstructor.getPropertyUtils();
+	        propertyUtils.setSkipMissingProperties(true);
+	        yamlConstructor.setPropertyUtils(propertyUtils);
+	        yaml = new Yaml(yamlConstructor);
+		}
+
+		protected BungeePluginLogger(Plugin plugin) {
+			super(plugin.getClass().getCanonicalName(), null);
+			this.pluginName = StringUtil.format("[{0}] ", this.getPluginName(plugin));
+			this.setParent(ProxyServer.getInstance().getLogger());
+		}
+
+		private String getPluginName(Plugin plugin) {
+			try (JarFile jar = new JarFile(plugin.getFile())) {
+				JarEntry pdf = jar.getJarEntry("bungee.yml");
+
+				if (pdf == null)
+					pdf = jar.getJarEntry("plugin.yml");
+
+				if (pdf == null)
+					return "BungeeCord";
+
+				try (InputStream in = jar.getInputStream(pdf)) {
+					PluginDescription desc = yaml.loadAs(in, PluginDescription.class);
+					return desc.getName();
+				}
+			} catch (Exception ex) {
+				return "BungeeCord";
+			}
+		}
+
+		@Override
+		public void log(LogRecord logRecord) {
+			logRecord.setMessage(this.pluginName + logRecord.getMessage());
+			super.log(logRecord);
+		}
+		
 	}
 
 }

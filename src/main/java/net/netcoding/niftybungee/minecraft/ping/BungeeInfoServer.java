6332096f5a63aa42c99640d5826b815beb9b9260
Brian Graham <support@netcoding.net>
2016-01-12 22:43:44 -0500

    Ignore RejectedExecutionExceptions, these occur because of fast enough pings to be started during a shutdown event.

--- src/main/java/net/netcoding/niftybungee/minecraft/ping/BungeeInfoServer.java
+++ src/main/java/net/netcoding/niftybungee/minecraft/ping/BungeeInfoServer.java
@@ -13,6 +13,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.RejectedExecutionException;
 
 public class BungeeInfoServer extends BukkitInfoServer {
 
@@ -53,33 +54,35 @@ public class BungeeInfoServer extends BukkitInfoServer {
 	public void ping() {
 		if (this.getAddress() == null) return;
 
-		this.serverInfo.ping(new Callback<ServerPing>() {
-			@Override
-			public void done(ServerPing result, Throwable error) {
-				reset();
+		try {
+			this.serverInfo.ping(new Callback<ServerPing>() {
+				@Override
+				public void done(ServerPing result, Throwable error) {
+					reset();
 
-				if (error != null) {
-					if (getName().equalsIgnoreCase("pixelmon") && getAddress().getAddress().getHostAddress().equals("192.99.45.103"))
-						System.out.println(StringUtil.format("Unable to ping {0}: {1}", getName(), error.getMessage()));
-				} else {
-					// Process Socket Registration
-					if (result.getVersion().getName().startsWith(NIFTY_PING) && BukkitHelper.getSocketWrapper().isSocketListening()) {
-						String niftyPing = StringUtil.split(",", result.getVersion().getName())[0];
-						int port = Integer.valueOf(StringUtil.split(" ", niftyPing)[1]);
-						onNiftyPing(port);
-						ping();
-						return;
+					if (error != null) {
+						if (getName().equalsIgnoreCase("pixelmon") && getAddress().getAddress().getHostAddress().equals("192.99.45.103"))
+							System.out.println(StringUtil.format("Unable to ping {0}: {1}", getName(), error.getMessage()));
+					} else {
+						// Process Socket Registration
+						if (result.getVersion().getName().startsWith(NIFTY_PING) && BukkitHelper.getSocketWrapper().isSocketListening()) {
+							String niftyPing = StringUtil.split(",", result.getVersion().getName())[0];
+							int port = Integer.valueOf(StringUtil.split(" ", niftyPing)[1]);
+							onNiftyPing(port);
+							ping();
+							return;
+						}
+
+						setVersion(result.getVersion().getName(), result.getVersion().getProtocol());
+						setMotd(result.getDescription());
+						setMaxPlayers(result.getPlayers().getMax());
+						setOnline(true);
 					}
 
-					setVersion(result.getVersion().getName(), result.getVersion().getProtocol());
-					setMotd(result.getDescription());
-					setMaxPlayers(result.getPlayers().getMax());
-					setOnline(true);
+					onPing();
 				}
-
-				onPing();
-			}
-		});
+			});
+		} catch (RejectedExecutionException ignore) { }
 	}
 
 	@Override

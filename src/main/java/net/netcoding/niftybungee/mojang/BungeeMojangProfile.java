f8bb616a07b28ad23210df5cd4cbb1660624dec8
Brian Graham <support@netcoding.net>
2016-04-17 23:10:59 -0400

    Fixed to support json message

--- src/main/java/net/netcoding/niftybungee/mojang/BungeeMojangProfile.java
+++ src/main/java/net/netcoding/niftybungee/mojang/BungeeMojangProfile.java
@@ -1,10 +1,13 @@
 package net.netcoding.niftybungee.mojang;
 
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.chat.ComponentSerializer;
 import net.netcoding.niftybungee.NiftyBungee;
 import net.netcoding.niftybungee.minecraft.ping.BungeeInfoServer;
 import net.netcoding.niftycore.mojang.MojangProfile;
+import net.netcoding.niftycore.util.json.JsonMessage;
 
 public class BungeeMojangProfile extends MojangProfile {
 
@@ -27,6 +30,16 @@ public class BungeeMojangProfile extends MojangProfile {
 	@Override
 	public boolean isOnlineAnywhere() {
 		return ProxyServer.getInstance().getPlayer(this.getUniqueId()) != null;
+	}
+
+	@Override
+	public void sendMessage(JsonMessage message) {
+		this.getPlayer().sendMessage(ComponentSerializer.parse(message.toJSONString()));
+	}
+
+	@Override
+	public void sendMessage(String message) {
+		this.getPlayer().sendMessage(TextComponent.fromLegacyText(message));
 	}
 
 }
